716cdf3bfb36726cd4db48415e08912cae7384df
Luis Morales <dev.luismorales@gmail.com>
2022-06-22 14:11:28 -0500

    se sincronizo las latitudes y longitudes del mapa con los valores en pantalla

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as  mapboxgl from 'mapbox-gl';
 
 
@@ -25,22 +25,29 @@ import * as  mapboxgl from 'mapbox-gl';
     ` 
   ]
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   mapa!: mapboxgl.Map;
 
   @ViewChild('mapa') divMapa!: ElementRef;
 
   zoomLevel: number = 10;
+  center: [number, number] = [ 7.911368174694034, 46.59012011926733 ];
 
   constructor() { }
+
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {});
+    this.mapa.off('zoomend', () => {});
+    this.mapa.off('move', () => {});
+  }
 
   ngAfterViewInit(): void {
 
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [ 7.911368174694034, 46.59012011926733 ],
+      center:this.center,
       zoom: this.zoomLevel
     });
 
@@ -52,6 +59,12 @@ export class ZoomRangeComponent implements AfterViewInit {
       if ( this.mapa.getZoom() > 18 ) {
         this.mapa.zoomTo(18);
       }
+    })
+    
+    this.mapa.on('move', (event) => {
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.center = [lng, lat]
     })
 
   }
