742a9e5b1333e24b5c055fd39d5d1bd3d867ddf6
Luis Morales <dev.luismorales@gmail.com>
2022-06-22 12:16:04 -0500

    se creo la estrutura para controlar el zoom del mapa

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import * as  mapboxgl from 'mapbox-gl';
+
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+    .mapa-container {
+      height: 100%;
+      width: 100%; 
+    }
+
+    .row {
+      background-color: white;
+      border-radius: 5px;
+      position: fixed;
+      bottom: 50px;
+      left: 50px;
+      padding: 10px;
+      z-index: 999;
+    }
+    ` 
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +29,14 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ 7.911368174694034, 46.59012011926733 ],
+      zoom: 14
+    });
+
   }
 
 }
