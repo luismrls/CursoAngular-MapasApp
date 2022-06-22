cfbf523d78c6da8f3d9a26e2a05b32c7ab6af791
Luis Morales <dev.luismorales@gmail.com>
2022-06-22 11:53:58 -0500

    se añadio el token temporal a los enverimet y se mostro el mapa fullscreen

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapBoxToken: 'pk.eyJ1IjoibWFnZGllbG1zIiwiYSI6ImNreXB6ZXJ1azBjeXEybnMyYm01bHk0aHAifQ.8iVI9ERFXi5ET09QbKJcww'
 };
